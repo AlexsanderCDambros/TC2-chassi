242c6434d3c747b2af12244de271f260a4143825
Alexsander Chagas Dambros <alexsanderdambros@gmail.com>
2025-08-01 00:02:50 -0300

    chore: tratativa para producao

---
+++ webpack.config.mjs
@@ -0,0 +1,27 @@
+import { merge } from 'webpack-merge';
+import singleSpaDefaults from 'webpack-config-single-spa';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+
+export default (webpackConfigEnv, argv) => {
+  const orgName = 'tc2';
+  const defaultConfig = singleSpaDefaults({
+    orgName,
+    projectName: 'root-config',
+    webpackConfigEnv,
+    argv,
+    disableHtmlGeneration: true,
+  });
+
+  return merge(defaultConfig, {
+    plugins: [
+      new HtmlWebpackPlugin({
+        inject: false,
+        template: 'src/index.ejs',
+        templateParameters: {
+          isLocal: webpackConfigEnv && webpackConfigEnv.isLocal,
+          orgName,
+        },
+      }),
+    ],
+  });
+};
